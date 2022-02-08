cb9d05d7eecaa79a1c8bb6c152c5cf8a5d844435
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-08 11:36:13 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startTime = 10;
+
+            while (startTime > 0) 
+            {
+                Console.WriteLine(startTime);
+                startTime--;
+            }
+        }
+    }
+}
